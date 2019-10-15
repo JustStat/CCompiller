5249a2613812f20e625b8ef46100938d792d8419
Kirill Varlamov <varlamov@farpost.com>
2019-10-15 19:03:39 +1000

    Initial Commit

---
+++ CCompiller/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CCompiller
+//
+//  Created by Kirill Varlamov on 15/10/2019.
+//  Copyright © 2019 Kirill Varlamov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
