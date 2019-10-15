be05976f010acf393f9704d77798fbcd1ef1288c
Kirill Varlamov <varlamov@farpost.com>
2019-10-15 21:08:08 +1000

    Add file reading

--- CCompiller/main.swift
+++ CCompiller/main.swift
@@ -8,7 +8,23 @@
 
 import Foundation
 
-for argument in CommandLine.arguments {
-    print(argument)
+let filePath = CommandLine.arguments[1]
+if !FileManager.default.fileExists(atPath: filePath) {
+    print("File not found at: \(filePath)")
+    exit(1)
 }
+
+guard let input = FileHandle(forReadingAtPath: filePath) else {
+    print("Can't get data from file at \(filePath)! Check permissions and try again!")
+    exit(1)
+}
+
+while true {
+    let data = input.readData(ofLength: 1)
+    if data.isEmpty {
+        break
+    }
+}
+
+
 
